0dab3a762cd131be42ec8022d13bf9cbdd3e9652
ouyangchengfeng <178915185@qq.com>
2026-01-09 18:08:18 +0800

    upgrade vapor

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v12)
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.121.0"),
     ],
     targets: [
         .target(
